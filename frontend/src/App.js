4ee7d528f63d03b720ece4fe7d68bac7e46a78fc
Santiago548 <dominicbarajas548@gmail.com>
2021-05-30 16:28:23 -0600

    added props and connect to my App.js file to show a loading or not loading phrase based on if loading is true or false. using a ternarary oporator.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './App.css';
 
-class App extends Component{
+class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
+        <header >
           <h1>Character APP</h1>
         </header>
-        <hr />
+        <ul>
+          {this.props.loading ? <h5>Loading Characters...</h5> : <h5>Not Loading...</h5>}
+        </ul>
+        <hr/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    characters: state.characterReducer.characters,
+    loading: state.characterReducer.loading
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
